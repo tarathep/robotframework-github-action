4f3b1667e03b4b2617a934dafce7e09d619de96f
tarathep <bokie.tarathep@gmail.com>
2021-08-23 12:10:03 +0700

    fix bash

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 docker run \
- -shm-size=$ALLOWED_SHARED_MEMORY \
+ --shm-size=$ALLOWED_SHARED_MEMORY \
  -e BROWSER=$BROWSER \
  -e ROBOT_THREADS=$ROBOT_THREADS \
  -e PABOT_OPTIONS=$PABOT_OPTIONS \
